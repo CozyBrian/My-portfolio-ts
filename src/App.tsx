dd17b40b9b7ce6cd66c82a5df81a4d926214d154
CozyBrian <dragonbrian33@gmail.com>
2022-08-29 15:19:18 +0000

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hehe New beginnings... I'm about to make the best portfolio ever
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
     </div>
   );
 }
