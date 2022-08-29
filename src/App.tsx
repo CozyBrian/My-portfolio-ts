274c3fcad489380367d6c215a882b2696b60efad
CozyBrian <dragonbrian33@gmail.com>
2022-08-29 14:11:48 +0000

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hehe New beginnings... I'm about to make the best portfolio ever
         </p>
         <a
           className="App-link"
