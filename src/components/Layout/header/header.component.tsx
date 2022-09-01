2a27c58fe5f67bc1521ea19f7f0c65104b23826e
CozyBrian <dragonbrian33@gmail.com>
2022-09-01 04:35:49 +0000

    responsive header

--- src/components/Layout/header/header.component.tsx
+++ src/components/Layout/header/header.component.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
 import useOnScreen from "../../../helpers";
 
 type Props = {
@@ -6,6 +7,8 @@ type Props = {
 };
 
 const Header = ({ pagerefs }: Props) => {
+  const [isOpen, setOpen] = useState(false);
+
   const isVisible = useOnScreen(pagerefs[0]);
   const isVisible1 = useOnScreen(pagerefs[1]);
   const isVisible2 = useOnScreen(pagerefs[2]);
@@ -25,10 +28,46 @@ const Header = ({ pagerefs }: Props) => {
   });
 
   return (
-    <div className="h-14 bg-[#000000a6] flex w-full z-50 items-center justify-center fixed top-0">
+    <div
+      className={`h-14 ${
+        !isOpen ? "bg-[#000000a6]" : "bg-black"
+      } flex w-full z-50 items-center justify-center fixed top-0`}
+    >
       <div className="container flex flex-row justify-between items-center h-full px-2 sm:px-24 text-gray-400">
         <h4 className="text-white opacity-100 text-xl">Brian Newton</h4>
-        <div className="flex flex-row justify-center items-center">
+        <div className="sm:hidden">
+          <button
+            onClick={() => setOpen(!isOpen)}
+            type="button"
+            className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium shadow-sm align-middle focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600 transition-all text-sm  hover:bg-slate-800 border-gray-700 text-gray-400 hover:text-white focus:ring-offset-gray-800"
+          >
+            {!isOpen ? (
+              <svg
+                className=" w-4 h-4"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                />
+              </svg>
+            ) : (
+              <svg
+                className=" w-4 h-4"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+              </svg>
+            )}
+          </button>
+        </div>
+        <div className="sm:flex flex-row justify-center items-center hidden overflow-hidden">
           <a
             href="#home"
             className="px-2 hover:text-gray-200 transition-all duration-100 hLink"
@@ -54,6 +93,49 @@ const Header = ({ pagerefs }: Props) => {
           </a>
         </div>
       </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            layout
+            className=" bg-[#000000a6] flex flex-col justify-start w-full z-10 fixed top-14 p-2 pb-4"
+            initial={{ y: -200 }}
+            animate={{ y: 0 }}
+            exit={{ y: -300 }}
+            transition={{ bounce: 1 }}
+          >
+            <a
+              href="#home"
+              className="p-2 text-gray-200 hover:text-slate-400 transition-all duration-100"
+              onClick={() => setOpen(!isOpen)}
+            >
+              Home
+            </a>
+            <a
+              href="#skills"
+              className="p-2 text-gray-200 hover:text-slate-400 transition-all duration-100"
+              onClick={() => setOpen(!isOpen)}
+            >
+              My Skills
+            </a>
+            <a
+              href="#projects"
+              className="p-2 text-gray-200 hover:text-slate-400 transition-all duration-100"
+              onClick={() => setOpen(!isOpen)}
+            >
+              Projects
+            </a>
+            <a
+              href="#contact"
+              rel="noopener noreferrer"
+              onClick={() => setOpen(!isOpen)}
+            >
+              <div className="flex items-center font-['Nunito_Sans'] justify-center p-1 m-2 duration-200 border border-sky-500 text-lg font-semibold rounded-md w-24 hover:bg-sky-500 text-sky-500 hover:text-gray-900">
+                Hire Me
+              </div>
+            </a>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
