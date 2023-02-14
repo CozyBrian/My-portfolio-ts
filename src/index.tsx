165e82ff2b1ec81adb44cb790a9e91493339d68f
CozyBrian <dragonbrian33@gmail.com>
2023-02-14 21:24:46 +0000

    added sitenary

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Sitenary from "sitenary";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+if (process.env.NODE_ENV !== "development") {
+  const sitenary = new Sitenary("63ebf9f3016df8a7928571db");
+}
+
 root.render(
   <React.StrictMode>
     <App />
