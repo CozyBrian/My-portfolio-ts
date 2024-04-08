f70cc2fe767a4e1d61f281df2fd70f6c2238f32c
CozyBrian <dragonbrian33@gmail.com>
2024-04-08 22:27:56 +0000

    add: dynamic date

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="w-full flex items-center justify-center py-4">
       <div className="w-full md:w-[880px] px-4 md:px-0 h-12 flex flex-row items-center justify-between">
         <p className="text-tesla-text text-sm font-normal">
-          © 2023 Brian Newton
+          © {new Date().getFullYear()} Brian Newton
         </p>
         <div className="w-fit h-10">
           <Image
